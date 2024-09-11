cb4a3a40626728e79e36f286782a1b82bda49317
samuelpd22 <samuel050101@hotmail.com>
2024-09-11 19:45:00 -0300

    feat : add contato c backend

---
+++ src/app/service/livros-service.service.ts
@@ -0,0 +1,29 @@
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Livros } from '../models/Livros';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LivrosServiceService {
+
+   //@Autowired
+   http = inject(HttpClient); //FAZ REQUISIÇÕES
+
+   API = "http://localhost:8080/livros";
+
+   constructor() { }
+
+   listAll(): Observable<Livros[]>{ //Metodo @GETMapping
+     return this.http.get<Livros[]>(this.API);
+   }
+
+
+   save(livro:Livros): Observable<string>{ //Metodo @PostMapping
+     return this.http.post<string>(this.API+"/enviar", livro  ,{ responseType: 'text' as 'json'})//Retorno string? Sempre usar responseType
+
+   }
+
+
+}
