f99afdabf32939194c93c30b2077f70bf4a9c3b9
samuelpd22 <samuel050101@hotmail.com>
2024-09-10 21:29:18 -0300

    initial commit

---
+++ src/app/components/conteudo/conteudo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conteudo',
+  standalone: true,
+  imports: [],
+  templateUrl: './conteudo.component.html',
+  styleUrl: './conteudo.component.scss'
+})
+export class ConteudoComponent {
+
+}
