cf4c8e0df66df6502b94b141d65f7ebcdbb2fbd8
samuelpd22 <samuel050101@hotmail.com>
2024-09-12 19:03:32 -0300

    feat : add novo atributo

--- src/app/models/Livros.ts
+++ src/app/models/Livros.ts
@@ -2,13 +2,15 @@ export class Livros {
   id!:number;
   titulo!:string;
   imagemUrl!:string;
+  genero!:string;
 
 
 
-  constructor(id:number,titulo:string ,imagemUrl:string){
+  constructor(id:number,titulo:string ,imagemUrl:string, genero:string){
     this.id = id;
     this.titulo = titulo;
     this.imagemUrl = imagemUrl;
+    this.genero = genero;
   }
 
 
